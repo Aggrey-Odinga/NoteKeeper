6ef419458f7e820acd7a6a78aaae50aaa3cee21a
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-06-30 11:59:28 +0300

    Populate data to the spinner

--- app/src/main/java/com/moringaschool/notekeeper/NoteActivity.java
+++ app/src/main/java/com/moringaschool/notekeeper/NoteActivity.java
@@ -1,10 +1,13 @@
 package com.moringaschool.notekeeper;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 
 public class NoteActivity extends AppCompatActivity {
@@ -17,7 +20,11 @@ public class NoteActivity extends AppCompatActivity {
 //            set variable for spinner
         Spinner spinnerCourses = findViewById(R.id.spinner_courses);
 //load data to populate on spinner
-
+//fetching courses list
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerCourses.setAdapter(adapterCourses);
 
 
     }
